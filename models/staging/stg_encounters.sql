2b5ef2543abb1369b39bfc656f3b11092c076e3b
Amy Finnegan <amy.finnegan@gmail.com>
2025-11-19 14:25:45 -0500

    Update stg_encounters.sql

--- models/staging/stg_encounters.sql
+++ models/staging/stg_encounters.sql
@@ -1,1 +1,7 @@
-
+select
+  encounter_id,
+  patient_id,
+  cast(admit_date as date) as admit_date,
+  cast(discharge_date as date) as discharge_date,
+  primary_dx
+from {{ source('readmissions_demo', 'encounters') }}
