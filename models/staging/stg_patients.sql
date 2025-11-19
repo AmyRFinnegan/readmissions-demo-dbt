3f93dc726718cc887138a7ddb0cd355d7e04c319
Amy Finnegan <amy.finnegan@gmail.com>
2025-11-19 15:22:54 -0500

    Update stg_patients.sql

--- models/staging/stg_patients.sql
+++ models/staging/stg_patients.sql
@@ -2,5 +2,5 @@ select
   patient_id,
   gender,
   cast(birth_date as date) as birth_date,
-  extract(year from date_diff(current_date(), birth_date, year)) as age
+  extract(year from date_diff(current_date(), cast(birth_date as date), year)) as age
 from {{ source('readmissions_demo', 'patients') }}
