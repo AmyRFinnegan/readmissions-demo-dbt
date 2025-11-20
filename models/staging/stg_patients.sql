7d427a8f603379487c4cddb7b5c6bc6b1ab6cb01
Amy Finnegan <amy.finnegan@gmail.com>
2025-11-20 09:18:57 -0500

    Fixing data issue

--- models/staging/stg_patients.sql
+++ models/staging/stg_patients.sql
@@ -1,13 +1,7 @@
 select
   patient_id,
   gender,
-  cast(birth_date as date) as birth_date,
-  extract(
-      year from date_diff(
-          current_date(),
-          cast(birth_date as date),
-          year
-      )
-  ) as age
+  birth_date,
+  date_diff(current_date(), birth_date, year) as age
 from {{ source('readmissions_raw', 'patients') }}
 
